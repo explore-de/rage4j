eb241fd4b42ccfe92f7f0458ec819d237bc3ae38
ris5266 <ris5266@users.noreply.github.com>
2025-02-28 15:10:14 +0100

    ✨ (rage4j-assert): add bleu/rouge score

---
+++ dev.rage4j.rage4j-assert/src/main/java/dev/rage4j/asserts/exception/Rage4JRougeScoreException.java
@@ -0,0 +1,15 @@
+package dev.rage4j.asserts.exception;
+
+public class Rage4JRougeScoreException extends RuntimeException
+{
+	public Rage4JRougeScoreException(String message)
+	{
+		super(message);
+	}
+
+	public Rage4JRougeScoreException()
+	{
+		// empty
+	}
+
+}
