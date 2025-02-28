e16298809ef48b4d1eb8bde0619965858bd91bfd
ris5266 <ris5266@users.noreply.github.com>
2025-02-28 15:10:14 +0100

    ✨ (rage4j-assert): add bleu/rouge score

--- dev.rage4j.rage4j-assert/src/main/java/dev/rage4j/asserts/RageAssertTestCaseAssertions.java
+++ dev.rage4j.rage4j-assert/src/main/java/dev/rage4j/asserts/RageAssertTestCaseAssertions.java
@@ -2,15 +2,19 @@ package dev.rage4j.asserts;
 
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.rage4j.asserts.exception.Rage4JBleuScoreException;
 import dev.rage4j.asserts.exception.Rage4JCorrectnessException;
 import dev.rage4j.asserts.exception.Rage4JFaithfulnessException;
 import dev.rage4j.asserts.exception.Rage4JRelevanceException;
+import dev.rage4j.asserts.exception.Rage4JRougeScoreException;
 import dev.rage4j.asserts.exception.Rage4JSimilarityException;
 import dev.rage4j.evaluation.Evaluation;
 import dev.rage4j.evaluation.answercorrectness.AnswerCorrectnessEvaluator;
 import dev.rage4j.evaluation.answerrelevance.AnswerRelevanceEvaluator;
 import dev.rage4j.evaluation.answersemanticsimilarity.AnswerSemanticSimilarityEvaluator;
+import dev.rage4j.evaluation.bleuscore.BleuScoreEvaluator;
 import dev.rage4j.evaluation.faithfulness.FaithfulnessEvaluator;
+import dev.rage4j.evaluation.rougescore.RougeScoreEvaluator;
 import dev.rage4j.model.Sample;
 
 import java.util.List;
@@ -102,5 +106,37 @@ public class RageAssertTestCaseAssertions
 			throw new Rage4JSimilarityException(MINVALUE + evaluation.getValue() + ", Required: " + minValue + ", Answer: " + answer);
 		}
 		return AssertionEvaluation.from(evaluation, this);
+	}
+
+	public Evaluation assertBleuScore(double minValue)
+	{
+		BleuScoreEvaluator evaluator = new BleuScoreEvaluator(embeddingModel);
+		Sample sample = Sample.builder()
+			.withAnswer(answer)
+			.withGroundTruth(groundTruth)
+			.build();
+		Evaluation evaluation = evaluator.evaluate(sample);
+
+		if (minValue > evaluation.getValue())
+		{
+			throw new Rage4JBleuScoreException(MINVALUE + evaluation.getValue() + ", Required: " + minValue + ", Answer: " + answer);
+		}
+		return evaluation;
+	}
+
+	public Evaluation assertRougeScore(double minValue, RougeScoreEvaluator.RougeType rougeType, RougeScoreEvaluator.MeasureType measureType)
+	{
+		RougeScoreEvaluator evaluator = new RougeScoreEvaluator(embeddingModel, rougeType, measureType);
+		Sample sample = Sample.builder()
+			.withAnswer(answer)
+			.withGroundTruth(groundTruth)
+			.build();
+		Evaluation evaluation = evaluator.evaluate(sample);
+
+		if (minValue > evaluation.getValue())
+		{
+			throw new Rage4JRougeScoreException(MINVALUE + evaluation.getValue() + ", Required: " + minValue + ", Answer: " + answer);
+		}
+		return evaluation;
 	}
 }
