0397b946bf21415eebeee72aa7b7775119445485
ris5266 <86254687+ris5266@users.noreply.github.com>
2025-11-26 16:13:18 +0100

    🚧 (bias): add refusal rate disparity

--- dev.rage4j.rage4j/src/test/java/dev/rage4j/evaluation/bias/RefusalEvaluatorIntegrationTest.java
+++ dev.rage4j.rage4j/src/test/java/dev/rage4j/evaluation/bias/RefusalEvaluatorIntegrationTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 @Tag("integration")
