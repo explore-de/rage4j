9097a2cc05f302109e4b48da841324a3f81666d9
Miggi <noreply@mymiggi.de>
2025-12-02 11:17:11 +0100

    ✨ (axcel): Enhance AxcelTempTest with DialogLoader for dynamic sample retrieval

--- dev.rage4j.rage4j/src/test/java/dev/rage4j/experiments/temperature/AxcelTempTest.java
+++ dev.rage4j.rage4j/src/test/java/dev/rage4j/experiments/temperature/AxcelTempTest.java
@@ -4,6 +4,7 @@ import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.rage4j.LoggingTestWatcher;
 import dev.rage4j.evaluation.Evaluation;
 import dev.rage4j.evaluation.axcel.AxcelEvaluator;
+import dev.rage4j.experiments.DialogLoader;
 import dev.rage4j.experiments.StatisticsUtil;
 import dev.rage4j.model.Sample;
 import org.junit.jupiter.api.AfterAll;
@@ -24,9 +25,11 @@ import static dev.langchain4j.model.chat.Capability.RESPONSE_FORMAT_JSON_SCHEMA;
 public class AxcelTempTest
 {
 	private static final String OPEN_AI_KEY = System.getenv("OPEN_AI_KEY");
-	private static final Map<String, List<Double>> results = new HashMap<>();
-	public static final int RUNS = 2;
-	private static final Logger log = LoggerFactory.getLogger(AxcelTempTest.class);
+	private static final Map<String, List<Double>> RESULTS = new HashMap<>();
+	private static final int RUNS = 10;
+	private static final Logger LOGGER = LoggerFactory.getLogger(AxcelTempTest.class);
+	private static final DialogLoader DIALOG_LOADER = new DialogLoader();
+
 	private AxcelEvaluator evaluator;
 
 	private static OpenAiChatModel buildChatModel(double temperature)
@@ -43,33 +46,31 @@ public class AxcelTempTest
 	@AfterAll
 	static void afterAll()
 	{
-		log.info("Evaluation results over tests:");
-		results.entrySet().stream()
+		LOGGER.info("Evaluation results over tests:");
+		RESULTS.entrySet().stream()
 			.map(StatisticsUtil::buildStats)
-			.forEach(stats -> log.info("{}", stats));
-	}
-
-	@RepeatedTest(RUNS)
-	@Tag("integration")
-	void temperature1Evaluation()
-	{
-		int temperature = 1;
-		evaluator = new AxcelEvaluator(buildChatModel(temperature));
-		Sample sample = ChatHistory.buildList();
-
-		Evaluation evaluation = evaluator.evaluate(sample);
-		results.computeIfAbsent("temperature" + temperature, k -> new ArrayList<>()).add(evaluation.getValue());
+			.forEach(stats -> LOGGER.info("{}", stats));
 	}
 
 	@RepeatedTest(RUNS)
 	@Tag("integration")
 	void temperature0Evaluation()
 	{
-		int temperature = 0;
-		evaluator = new AxcelEvaluator(buildChatModel(temperature));
-		Sample sample = ChatHistory.buildList();
+		Sample sample = DIALOG_LOADER.getDialog();
+		runTestWithTemperature(1, sample);
+		runTestWithTemperature(0, sample);
+	}
 
+	private void runTestWithTemperature(int temperature, Sample sample)
+	{
+		// given
+		evaluator = new AxcelEvaluator(buildChatModel(temperature));
+
+		// when
 		Evaluation evaluation = evaluator.evaluate(sample);
-		results.computeIfAbsent("temperature" + temperature, k -> new ArrayList<>()).add(evaluation.getValue());
+
+		// then
+		String mapKey = "temperature" + temperature;
+		RESULTS.computeIfAbsent(mapKey, k -> new ArrayList<>()).add(evaluation.getValue());
 	}
 }
